7eefe35ebdcdd254368d3fc036e5f1c5ba78b5da
Dominik Grabiec <dominik.grabiec@monaco.net.pl>
2026-02-23 20:48:15 +0100

    test 2

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/hades/',
+  base: '/hades/',
   server: {
     port: 5000,
   },
