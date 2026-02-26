80e13334462d70cc72993b7c68eb8a6d66268f7f
Dominik Grabiec <dominik.grabiec@monaco.net.pl>
2026-02-26 14:21:33 +0100

    typo

--- src/components/DogTricks.vue
+++ src/components/DogTricks.vue
@@ -6,7 +6,7 @@ const tricks = [
     name: 'Skupienie uwagi',
     command: '-',
     description:
-      'Wyciągamy smaczka w bok, pies powinien wtedy spojrzeć na nas następnie nagradzmay go smaczkiem',
+      'Wyciągamy smaczek w bok, pies powinien wtedy spojrzeć na nas, następnie nagradzamy go smaczkiem.',
     ytLink: 'https://www.youtube.com/watch?v=K1EWibyPqqU&t=1s',
     film: '',
   },
@@ -14,7 +14,7 @@ const tricks = [
     name: 'Do mnie',
     command: 'Do mnie',
     description:
-      'Wołamy "Do mnie", jeżeli przybiega dajemy mu smaczek i chwalimy go. Warto robić w różnych odlegościach i wołać go jak nie jest na nas sfokusowany.',
+      'Wołamy "Do mnie", jeżeli przybiega, dajemy mu smaczek i chwalimy go. Warto robić w różnych odległościach i wołać go, gdy nie jest na nas skupiony.',
     ytLink: 'https://www.youtube.com/watch?v=lDCmeGN9sgg&t=1s',
     film: '',
   },
@@ -22,14 +22,14 @@ const tricks = [
     name: 'Siad',
     command: 'Siad',
     description:
-      'Pies powinien wykonać komendę na samo słowo siad, jeżeli tego nie robi dodać gest palca w dół jak na filmie.',
+      'Pies powinien wykonać komendę na samo słowo "siad", jeżeli tego nie robi, dodać gest palca w dół jak na filmie.',
     ytLink: 'https://www.youtube.com/watch?v=OnLbb8RimWg',
     film: '',
   },
   {
     name: 'Leżeć',
     command: 'Leżeć',
-    description: 'Pies umie kłaść się na komende, jeżei tego nie robi dodać - pozioma dłoń.',
+    description: 'Pies umie kłaść się na komendę, jeżeli tego nie robi, dodać poziomą dłoń.',
     ytLink: 'https://www.youtube.com/watch?v=kKxDjZ7rl0ns',
     film: '',
   },
@@ -37,7 +37,7 @@ const tricks = [
     name: 'Łapa',
     command: 'Łapa + druga łapa',
     description:
-      'Podstawiamy rękę pod jego łapę jak stoi/siedzi i wydajemy komendę łapa jak poda nagradzamy smaczkiem, na zmianę z sekwensją łapa + druga łapa i dopiero wtedy dajemy smaczek.',
+      'Podstawiamy rękę pod jego łapę, gdy stoi/siedzi i wydajemy komendę "łapa", gdy poda, nagradzamy smaczkiem. Na zmianę z sekwencją "łapa + druga łapa" i dopiero wtedy dajemy smaczek.',
     ytLink: 'https://www.youtube.com/watch?v=v1VNhQSjqUY',
     film: '',
   },
@@ -45,7 +45,7 @@ const tricks = [
     name: 'Proszenie o smakołyk',
     command: 'Proś',
     description:
-      'Pies podskakuje na tą komenddę i prosi, połowicznie umie na samą komende warto trenować z gestem stopniowo go wycofując.',
+      'Pies podskakuje na tę komendę i prosi, połowicznie umie na samą komendę, warto trenować z gestem, stopniowo go wycofując.',
     ytLink: 'https://www.youtube.com/watch?v=sIxGgXbz_kA&t=149s',
     film: '',
   },
@@ -53,7 +53,7 @@ const tricks = [
     name: 'Czekaj',
     command: 'Czekaj',
     description:
-      'Pies siada i czeka na komędę do mnie. Wydłużać czas czekania z każdym powtórzeniem. Warto powiedzieć komendę, odejść wrócić do psa po x czasu nagrodzić go wydać znowu komendę odejśc na jakiś czas i go zawołać, jak przybiegnie nagrodzić.',
+      'Pies siada i czeka na komendę "do mnie". Wydłużać czas czekania z każdym powtórzeniem. Warto powiedzieć komendę, odejść, wrócić do psa po jakimś czasie, nagrodzić go, wydać znowu komendę, odejść na jakiś czas i go zawołać, gdy przybiegnie, nagrodzić.',
     ytLink: 'https://www.youtube.com/watch?v=31CprFHWo78&t=139s',
     film: '',
   },
@@ -61,7 +61,7 @@ const tricks = [
     name: 'Na miejsce',
     command: 'Na miejsce',
     description:
-      'Należy palcem wskazać miejsce na którę ma iśc pies, powinien się położyć i czekać na komendę do mnie. Na początku nagradzać za samo pojście na swoje miejsce. Odejśc na jakiś czas (na razie wytrzymuje do 1 min), wrócić i nagrodzić psa potem znowu odejśc i go zawołać wraz z nagrodą za przyjście.',
+      'Należy palcem wskazać miejsce, na które ma iść pies, powinien się położyć i czekać na komendę "do mnie". Na początku nagradzać za samo pójście na swoje miejsce. Odejść na jakiś czas (na razie wytrzymuje do 1 min), wrócić i nagrodzić psa, potem znowu odejść i go zawołać wraz z nagrodą za przyjście.',
     ytLink: 'https://www.youtube.com/watch?v=StgN2DQuhao',
     film: '',
   },
@@ -69,7 +69,7 @@ const tricks = [
     name: 'Noga',
     command: 'Noga',
     description:
-      'Pies po wydaniu tej komendy powiinien chodzić przy prawej nodze, ważne żeby nakierować go smaczkiem na dobrą nogę jeżeli idzie po złej. Nagradzać co jakiś czas (np: 15s czy 30s) za chodzenie przy nodze. Tak samno na spacerze jak sam z siebie chodzi blisko również nagrodzić.',
+      'Pies po wydaniu tej komendy powinien chodzić przy prawej nodze, ważne, żeby nakierować go smaczkiem na dobrą nogę, jeżeli idzie po złej. Nagradzać co jakiś czas (np. 15 s czy 30 s) za chodzenie przy nodze. Tak samo na spacerze, gdy sam z siebie chodzi blisko, również nagrodzić.',
     ytLink: 'https://www.youtube.com/watch?v=Gd_rx9K8aW4&t=149s',
     film: '',
   },
@@ -77,7 +77,7 @@ const tricks = [
     name: 'Środek',
     command: 'Środek',
     description:
-      'Pies po tej komendzie i geście od tyłu wchodzi pomiędzy nogami i siada patrząc na nas. Próbować również bez gestu.',
+      'Pies po tej komendzie i geście od tyłu wchodzi pomiędzy nogami i siada, patrząc na nas. Próbować również bez gestu.',
     ytLink: 'https://www.youtube.com/watch?v=XqpOum5r7OY&t=81s',
     film: '',
   },
@@ -85,7 +85,7 @@ const tricks = [
     name: 'Slalom',
     command: 'Slalom',
     description:
-      'Pies przechodzi pomiędzy nogami, pamiętać żeby zacząć od dobrej nogi, od przeciwnej niż po której stoi pies. Nagradzać go po przejściu sekwencji 4 przejść.',
+      'Pies przechodzi pomiędzy nogami, pamiętać, żeby zacząć od dobrej nogi, od przeciwnej niż po której stoi pies. Nagradzać go po przejściu sekwencji 4 przejść.',
     ytLink: 'https://www.youtube.com/watch?v=S2Jox8tKOL4',
     film: '',
   },
@@ -93,7 +93,7 @@ const tricks = [
     name: 'Zostaw',
     command: 'Zostaw',
     description:
-      'Podczas zabawy z psem szarpakiem  przestać szarpać i wydać komendę zostaw pies powinien się uspokoić i oddać zabawkę, po oddaniu zabawki bawić się z nim dalej tą samą zabawką. Powtarzać kilka razy przy zabawie.',
+      'Podczas zabawy z psem szarpakiem przestać szarpać i wydać komendę "zostaw", pies powinien się uspokoić i oddać zabawkę, po oddaniu zabawki bawić się z nim dalej tą samą zabawką. Powtarzać kilka razy przy zabawie.',
     ytLink: 'https://www.youtube.com/watch?v=9g9thndLB7A',
     film: '',
   },
@@ -101,7 +101,7 @@ const tricks = [
     name: 'Jedzenie',
     command: 'Jedzenie',
     description:
-      'Komenda informująca psa, że zaraz dostanie jedzienie. Komende powiedzieć przed nasypaniem do miski jedzenia. Pies po położeniu miski powinienn czekać na komędę jeżlei tego nie zrobi zakryć jedzenie nogą i czekać aż na nas spoojrzy. Sprawdzać psa co jakiś czas mówiąć rożne słowa bardziej lub mniej zbliżone do słowa komendy.',
+      'Komenda informująca psa, że zaraz dostanie jedzenie. Komendę powiedzieć przed nasypaniem do miski jedzenia. Pies po położeniu miski powinien czekać na komendę, jeżeli tego nie zrobi, zakryć jedzenie nogą i czekać, aż na nas spojrzy. Sprawdzać psa co jakiś czas, mówiąc różne słowa bardziej lub mniej zbliżone do słowa komendy.',
     ytLink: '',
     film: '',
   },
@@ -117,14 +117,14 @@ const tricks = [
     name: 'Ukłon',
     command: 'Ukłon',
     description:
-      'Pies klania sie przednimi łapkami na tą komędę wraz z gestem. Stopniowo zmiejszać gest tak aby na koniec wykjonywał ją jedynie poprzez komende.',
+      'Pies kłania się przednimi łapkami na tę komendę wraz z gestem. Stopniowo zmniejszać gest tak, aby na koniec wykonywał ją jedynie poprzez komendę.',
     ytLink: 'https://www.youtube.com/watch?v=OfzE-dJ4jAs',
     film: '',
   },
   {
     name: 'Obrót',
     command: 'Obrót',
-    description: 'Pies wykonuje tą komendę poprzez ruch ręką w okół niego.',
+    description: 'Pies wykonuje tę komendę poprzez ruch ręką wokół niego.',
     ytLink: 'https://www.youtube.com/shorts/-j40M-wcdDI',
     film: '',
   },
@@ -132,7 +132,7 @@ const tricks = [
     name: 'Pozwolenie',
     command: 'Proszę',
     description:
-      'Kładziemy na ręku smaczka i czekamy aż pies będzie na nas patrzył i nie będzie zainteresowany smaczkiem. Wydajemy komędę pozwolenia i dajemy mu smaczka. Tak samo ćwiczymy na zabawkach. Jeżeli próbuje zabrać smaczka lub chwycić zabawkę zamykamy rękę lub oddalamy powoli od niego.',
+      'Kładziemy na ręce smaczek i czekamy, aż pies będzie na nas patrzył i nie będzie zainteresowany smaczkiem. Wydajemy komendę pozwolenia i dajemy mu smaczek. Tak samo ćwiczymy na zabawkach. Jeżeli próbuje zabrać smaczek lub chwycić zabawkę, zamykamy rękę lub oddalamy powoli od niego.',
     ytLink: '',
     film: '',
   },
@@ -140,7 +140,7 @@ const tricks = [
     name: 'Spacer',
     command: 'Spacer',
     description:
-      'Komenda "Czy" "chcesz" "iść" "na" "spacer" wykonana pojedyńczymy słowami zachęca psa żeby pójśc na spacer. Przed samym wyjściem powtórzyć komendę "spacer". Przed założeniem obroży należy wydać komendę siad i dopiero wtedy założyć psu obrożę i z nim wyjść. Adekwatnie powrót.',
+      'Komenda "Czy", "chcesz", "iść", "na", "spacer" wykonana pojedynczymi słowami zachęca psa, żeby pójść na spacer. Przed samym wyjściem powtórzyć komendę "spacer". Przed założeniem obroży należy wydać komendę "siad" i dopiero wtedy założyć psu obrożę i z nim wyjść. Adekwatnie powrót.',
     ytLink: '',
     film: '',
   },
@@ -148,7 +148,7 @@ const tricks = [
 
 const newTricks = [
   {
-    name: 'Skok przez rękę/przeskodę',
+    name: 'Skok przez rękę/przeszkodę',
     command: 'Hop',
     description: '',
     descriptionLink: 'https://johndog.pl/blog/wychowanie/skok-przez-przeszkode/',
@@ -166,12 +166,12 @@ const newTricks = [
 ]
 
 const list = [
-  'Codzinnie powtarzać z psem komendy przez 10 - 15 min 2/3 razy dziennie.',
-  'WAŻNE!!! Po każdej prawidłowo wykonanej komendzie mówimy "SUPER" a następnie nagardzamy smaczkiem. Słowo "Super" powinno być wypowiedziane jak najszybciej po prawidłowym zachowaniu psa, żeby wiedział za co został nagrodzony.',
-  'Każdą komendę wykonać kilka razy po sobie dać smaczka przy piewrwszym wykonaniu oraz ostanim w środkowcyh powtórzeniach nie zawsze dawać jeżlei idzie my lepiej dawać żadziej.',
-  'Komendy wykonywać w różnych miejscach i sytuacjach żeby pies się nie przyzwyczajał do nich tylko w konkretnych warunkach.',
-  'Podczas spacerów codzieennie ćwiczyć komende do mnie, noga oraz czekaj i nagradzać go smaczkiem (warto brać mokre smaczki na spacery wtedy lepiej słucha).',
-  'Komendy wydawać raz wraz z gestem, jeżeli nie wykona komendy odejść na chwilę i spróbować ponownie.',
+  'Codziennie powtarzać z psem komendy przez 10-15 min, 2-3 razy dziennie.',
+  'WAŻNE!!! Po każdej prawidłowo wykonanej komendzie mówimy "SUPER", a następnie nagradzamy smaczkiem. Słowo "Super" powinno być wypowiedziane jak najszybciej po prawidłowym zachowaniu psa, żeby wiedział, za co został nagrodzony.',
+  'Każdą komendę wykonać kilka razy pod rząd. Dać smaczka przy pierwszym i ostatnim wykonaniu, w środkowych powtórzeniach nie zawsze dawać — jeżeli idzie mu lepiej, nagradzać rzadziej.',
+  'Komendy wykonywać w różnych miejscach i sytuacjach, żeby pies nie przyzwyczajał się do nich tylko w konkretnych warunkach.',
+  'Podczas spacerów codziennie ćwiczyć komendy "do mnie", "noga" oraz "czekaj" i nagradzać go smaczkiem (warto brać mokre smaczki na spacery — wtedy lepiej słucha).',
+  'Komendy wydawać raz wraz z gestem. Jeżeli nie wykona komendy, odejść na chwilę i spróbować ponownie.',
 ]
 </script>
 
